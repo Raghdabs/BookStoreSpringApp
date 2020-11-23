740ce21bc76a44983fb03d22bfdda8fd707d42d2
Malek <malek.benyedder792@gmail.Com>
2020-11-23 22:36:18 +0100

    First step

---
+++ src/main/java/com/sip/ams/entities/Book.java
@@ -0,0 +1,63 @@
+package com.sip.ams.entities;
+
+import java.sql.Date;
+
+public class Book {
+	private int id;
+    private String title;
+    private String Author;
+    private double Price;
+    private Date ReleaseDate;
+    private String path;
+	public Book(int id, String title, String author, double price, Date releaseDate, String path) {
+		super();
+		this.id = id;
+		this.title = title;
+		Author = author;
+		Price = price;
+		ReleaseDate = releaseDate;
+		this.path = path;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author = author;
+	}
+	public double getPrice() {
+		return Price;
+	}
+	public void setPrice(double price) {
+		Price = price;
+	}
+	public Date getReleaseDate() {
+		return ReleaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		ReleaseDate = releaseDate;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", Author=" + Author + ", Price=" + Price + ", ReleaseDate="
+				+ ReleaseDate + ", path=" + path + "]";
+	}
+	
+}
---
+++ src/main/java/com/sip/ams/entities/Command.java
@@ -0,0 +1,90 @@
+package com.sip.ams.entities;
+
+import java.sql.Date;
+
+public class Command {
+	
+	private int id;
+    private double prix;
+    private Date Dcommande;
+    private int idClient;
+    private String nom;
+    private String  prenom;
+    private String email;
+    private int tel;
+    private String adress;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public Date getDcommande() {
+		return Dcommande;
+	}
+	public void setDcommande(Date dcommande) {
+		Dcommande = dcommande;
+	}
+	public int getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(int idClient) {
+		this.idClient = idClient;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public Command(int id, double prix, Date dcommande, int idClient, String nom, String prenom, String email, int tel,
+			String adress) {
+		super();
+		this.id = id;
+		this.prix = prix;
+		Dcommande = dcommande;
+		this.idClient = idClient;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.tel = tel;
+		this.adress = adress;
+	}
+	@Override
+	public String toString() {
+		return "Command [id=" + id + ", prix=" + prix + ", Dcommande=" + Dcommande + ", idClient=" + idClient + ", nom="
+				+ nom + ", prenom=" + prenom + ", email=" + email + ", tel=" + tel + ", adress=" + adress + "]";
+	}
+    
+    
+}
