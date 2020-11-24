b9fb0cb3e64fafd94acccb3ad165801c466c8e75
Malek <malek.benyedder792@gmail.Com>
2020-11-24 15:11:17 +0100

    Fix

--- src/main/java/com/sip/ams/entities/Book.java
+++ src/main/java/com/sip/ams/entities/Book.java
@@ -1,4 +1,4 @@
-package com.sip.demo.entities;
+package com.sip.ams.entities;
 import java.sql.Date;
 
 import javax.persistence.Column;
@@ -9,79 +9,7 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 @Entity
 public class Book {
-<<<<<<< HEAD
-	private int id;
-	private String title;
-	private String Author;
-	private double Price;
-	private Date ReleaseDate;
-	private String path;
 
-	public Book(int id, String title, String author, double price, Date releaseDate, String path) {
-		super();
-		this.id = id;
-		this.title = title;
-		Author = author;
-		Price = price;
-		ReleaseDate = releaseDate;
-		this.path = path;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return Author;
-	}
-
-	public void setAuthor(String author) {
-		Author = author;
-	}
-
-	public double getPrice() {
-		return Price;
-	}
-
-	public void setPrice(double price) {
-		Price = price;
-	}
-
-	public Date getReleaseDate() {
-		return ReleaseDate;
-	}
-
-	public void setReleaseDate(Date releaseDate) {
-		ReleaseDate = releaseDate;
-	}
-
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
-
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", Author=" + Author + ", Price=" + Price + ", ReleaseDate="
-				+ ReleaseDate + ", path=" + path + "]";
-	}
-
-=======
 	
 @Id
  @GeneratedValue(strategy = GenerationType.AUTO)
@@ -128,9 +56,10 @@ public void setTitle(String title) {
 	this.title = title;
 }
 
+
+
 public String getAuthor() {
 	return author;
->>>>>>> 96ccd0ea73462efe47bf626a38495905cb0b5e10
 }
 
 public void setAuthor(String author) {
